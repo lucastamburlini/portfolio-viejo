acb70215dd451ac673e600617c0a28be13b6f7a3
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-04-26 20:28:54 -0300

    Modificación "Herramientas"

--- src/pages/main/Tools.jsx
+++ src/pages/main/Tools.jsx
@@ -6,9 +6,16 @@ const Tools = () => {
   return (
     <section className="tools-container">
       {tools.map((tool) => (
-        <div key={tool.name} className="tool-card">
-          <div className="tool-icon">{tool.icon}</div>
-          <h2 className="tool-name">{tool.name}</h2>
+        <div key={tool.category}>
+          <h1>{tool.category}</h1>
+          <div className="tool-skill-container">
+            {tool.items.map((item) => (
+              <div className="tool-items" key={item.name}>
+                <div className="tool-icon">{item.icon}</div>
+                <h2 className="tool-name">{item.name}</h2>
+              </div>
+            ))}
+          </div>
         </div>
       ))}
     </section>
